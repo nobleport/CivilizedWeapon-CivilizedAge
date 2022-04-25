9d5c7a311becdc471d36154ed9fab3049310cd1a
John Gardner <john.s.gardner@gmail.com>
2022-04-24 20:57:24 -0700

    Implement basic structure for workbench page

--- src/pages/workbench-page.js
+++ src/pages/workbench-page.js
@@ -11,11 +11,13 @@ export default class Workbench {
         let workbenchPageFigure = document.createElement("figure");
         workbenchPageFigure.id = 'workbench';
         document.body.appendChild(workbenchPageFigure);
+        // this.addBackgroundImg(workbenchPageFigure);
         let backButton = document.createElement("button");
         backButton.id = "back-button";
         workbenchPageFigure.appendChild(backButton)
         backButton.innerHTML = "Back";
         backButton.addEventListener("click", this.toWelcomeScreen.bind(this))
+        this.constructSidebar(workbenchPageFigure);
     }
     
     toWelcomeScreen(){
@@ -23,5 +25,46 @@ export default class Workbench {
         document.body.removeChild(workbenchFigure)
         new Welcome();
     }
+
+    // addBackgroundImg(workbenchPageFigure){
+    //     let bgImg = document.createElement("div");
+    //     bgImg.className = "workbenchBackground";
+    //     workbenchPageFigure.appendChild(bgImg);
+    // }
+
+    constructSidebar(workbenchPageFigure){
+        let sidebar = document.createElement("div");
+        sidebar.className = "workbench-sidebar"
+        workbenchPageFigure.appendChild(sidebar);
+        let ul = document.createElement("ul");
+        sidebar.appendChild(ul)
+        this.addCrystalOptions(ul);
+        this.addSleeveOptions(ul);
+        this.addEmitterOptions(ul);
+    }
+
+    addCrystalOptions(ul){
+        let button = document.createElement("button");
+        button.className = "crystal";
+        button.innerHTML = "Crystals"
+        ul.appendChild(button);
+
+    }
+
+    addSleeveOptions(ul){
+        let button = document.createElement("button");
+        button.className = "sleeve";
+        button.innerHTML = "Sleeves"
+        ul.appendChild(button);
+    }
+
+    addEmitterOptions(ul){
+        let button = document.createElement("button");
+        button.className = "emitter";
+        button.innerHTML = "Emitters"
+        ul.appendChild(button);
+    }
+
+    
 }
 // module.exports = Workbench;
