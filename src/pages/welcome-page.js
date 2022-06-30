118a8dc263916e5bb52be9b4e9faa39e3b6fd9b1
John Gardner <john.s.gardner@gmail.com>
2022-06-30 13:20:58 -0700

    Add about me information

--- src/pages/welcome-page.js
+++ src/pages/welcome-page.js
@@ -26,6 +26,27 @@ export default class Welcome {
             li4.className = "launch-button";
             //this should link us to generating the workbench page//
             ul.appendChild(li4)
+        let li5 = document.createElement('div');
+            li5.className = 'about-me';
+            ul.appendChild(li5);
+        let personalWeb = document.createElement( 'a');
+            personalWeb.className = 'github';
+            personalWeb.href = '';
+            personalWeb.target = '_blank';
+            personalWeb.innerHTML = 'About Me';
+            li5.appendChild(personalWeb); 
+        let github = document.createElement('a');
+            github.className = 'github';
+            github.href = 'https://github.com/nobleport/ElegantWeapon-CivilizedAge'
+            github.target = '_blank'
+            github.innerHTML = 'Github';
+            li5.appendChild(github);
+        let linkedIn = document.createElement('a');
+            linkedIn.className = 'linked-in'
+            linkedIn.href = 'https://www.linkedin.com/in/john-gardner-320b4a20b'
+            linkedIn.innerHTML = 'LinkedIn';
+            linkedIn.target='_blank'
+            li5.appendChild(linkedIn);
         li4.addEventListener("click", () => {
             let soundEffect = new Audio("images/lightspeed.mp3");
             soundEffect.play()
@@ -40,7 +61,7 @@ export default class Welcome {
         li3.innerHTML = 'Before you can be fully accepted into the Jedi Order, you must construct your first lightsaber prototype'
         li3.innerHTML += '. Think carefully about your design. Remember, there are consequences to every choice, and the choices '
         li3.innerHTML += 'you make now will help determine your focus as a Jedi.\
-Select a sleeve, then a crystal, then cap it off with an emitter. May the Force be with you.'
+ Select a sleeve, then a crystal, then cap it off with an emitter. May the Force be with you.'
         li4.innerHTML = "Launch"
     }
 
